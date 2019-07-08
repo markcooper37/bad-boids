0c6c656f3f0b3cfc7363ca576bb7280f5b079fdc
Mark Cooper <cooperm1700@hotmail.co.uk>
2019-07-08 14:39:34 +0100

    Size added

--- boids.py
+++ boids.py
@@ -8,12 +8,13 @@ from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
-# Alteration
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+size = 50
+
+boids_x=[random.uniform(-450,50.0) for x in range(size)]
+boids_y=[random.uniform(300.0,600.0) for x in range(size)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(size)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(size)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
